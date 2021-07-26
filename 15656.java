f4df2444aa4ffa8b75c11d51100ba5aa69f2169b
heerachoi <heera7722@gmail.com>
2021-07-26 23:44:15 +0900

    N과 M (7)

---
+++ 15656.java
@@ -0,0 +1,46 @@
+// 문제링크: https://www.acmicpc.net/problem/15656
+// 문제설명: https://coder-design.tistory.com/entry/%EB%B0%B1%EC%A4%80-15656-N%EA%B3%BC-M-6-JAVA%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+                result[depth] = arr[i];
+                dfs(depth + 1);
+        }
+    }
+}
+
