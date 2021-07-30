d29d5dadc6a574a5549201312fd828397cb2b977
heerachoi <heera7722@gmail.com>
2021-07-30 11:08:28 +0900

    바이러스

--- 2606.java
+++ 2606.java
@@ -1,45 +1,52 @@
+// 문제 설명
+// https://coder-design.tistory.com/entry/%EB%B0%B1%EC%A4%80-2606-%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4-JAVABFS
+import java.io.*;
 import java.util.*;
 
-
-
 public class Main {
-    static int node[][]; // 그래프 배열
-    static int check[]; // 방문 배열
-    static void bfs(int start) { // BFS 메소드
-        Queue<Integer> queue = new LinkedList<>();
 
-        check[start] =1;
-        queue.offer(start);
-        int cnt = 0; // 감염 된 컴퓨터의 수
-        while(!queue.isEmpty()) {
-            int x = queue.poll();
-            System.out.println(" ---- x ----- " + x);
-            for(int i=1;i<node.length;i++) { 
-                if(node[x][i]==1 && check[i]!=1) {
-                    queue.offer(i);
-                    check[i] = 1;
+    static ArrayList<Integer> arr[];
+    static int check[];
+    static int cnt=0;
+    static ArrayList<Integer> result =new ArrayList<>();
+    static StringTokenizer st;
+    static int map[];
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+
+        map = new int[N+1];
+        ArrayList<Integer> arr[] = new ArrayList[N+1];
+        for(int i = 1 ; i < N+1 ; i++) { // 열결 상태 저장
+            arr[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < T ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            arr[from].add(to);
+            arr[to].add(from);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        map[1] = 1;
+        int cnt =0 ;
+
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            for(int i = 0; i < arr[now].size() ; i++) {
+                int next = arr[now].get(i);
+                if(map[next] != 1) { // 방문하지 않았다
+                    map[next] = 1;
+                    q.add(next);
                     cnt++;
-                    System.out.println("cnt == " + cnt);
                 }
             }
         }
-        System.out.println(cnt); //모든 탐색을 마치고 cnt 출력
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n= sc.nextInt(); // 컴퓨터의 수
-        int m = sc.nextInt(); 
-
-        node=new int[n+1][n+1];
-        check = new int[n+1];
-        for(int i=0;i<m;i++) { // 그래프 구성
-            int a=sc.nextInt();
-            int b = sc.nextInt();
-            node[a][b]=1;
-            node[b][a]=1;
-
-        }
-        bfs(1);
+        System.out.println(cnt);
     }
 }
